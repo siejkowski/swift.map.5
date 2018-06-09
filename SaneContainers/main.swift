3950c5fea65afc314bd6638d53021528aa026c8f
Krzysztof Siejkowski <krzysztof@siejkowski.net>
2018-06-09 05:57:57 +0200

    Initial Commit

---
+++ SaneContainers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SaneContainers
+//
+//  Created by Krzysztof Siejkowski on 09/06/2018.
+//  Copyright © 2018 Krzysztof Siejkowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
